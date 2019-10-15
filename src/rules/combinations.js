424c6dfe1afb0b2828aa9b16135df852c4a975d0
Justin Jenkins <jjenkins@churchcommunitybuilder.com>
2019-10-15 14:11:33 -0600

    Add rules/combinations

---
+++ src/rules/combinations.js
@@ -0,0 +1,139 @@
+// @todo should we add in the chance to get another roll
+
+export const addAllDice = dice => { const add = (a,b) => a+b; return dice.reduce(add); };
+
+export const addNumberedDice = (dice, number) => {
+    const matchingDice = dice.filter( di => di === number);
+    return matchingDice.length ? addAllDice(matchingDice) : 0;
+};
+
+const combos = {
+    "number": { 
+        "points": (dice, number) => addNumberedDice(dice, number), 
+        "min": 0, "match": true, "assending": false
+    },
+    "three_of_a_kind": { 
+        "points": dice => addAllDice(dice), 
+        "min": 3, "match": true, "assending": false 
+    },
+    "four_of_a_kind": { 
+        "points": dice => addAllDice(dice), 
+        "min": 4, "match": true, "assending": false 
+    },
+    "full_house": { 
+        "points": dice => 25, 
+        "min": 2, "match": true, "assending": false 
+    },
+    "small_straight": { 
+        "points": dice => 30, 
+        "min": 4, "match": false, "assending": true 
+    },
+    "large_straight": { 
+        "points": dice => 40, 
+        "min": 5, "match": false, "assending": true 
+    },
+    "yahtzee": { 
+        "points": dice => 50, 
+        "min": 5, "match": true, "assending": false 
+    },
+    "chance": { 
+        "points": dice => addAllDice(dice), 
+        "min": 0, "match": false, "assending": false
+    },
+};
+
+export const determinePoints = (type, dice, number) => {
+
+    dice.sort();
+
+    let points = 0;
+
+    if (
+        (type === "full_house" && fullHouse(dice)) ||
+        (type === "small_straight" && straight(dice, combos.small_straight.min)) ||
+        (type === "large_straight" && straight(dice, combos.large_straight.min)) ||
+        (type === "three_of_a_kind" && ofAKind(dice,combos.three_of_a_kind.min)) ||
+        (type === "four_of_a_kind" && ofAKind(dice, combos.four_of_a_kind.min)) ||
+        (type === "yahtzee" && yahtzee(dice)) ||
+        (type === "chance") || 
+        (type === "number" && number)
+    ){
+        return number ? combos[type].points(dice, number) : combos[type].points(dice);
+    }
+
+    return points;
+
+};
+
+const getMatches = (dice, min) => {
+
+    dice.sort();
+
+    const diceGrouped = groupDice(dice);
+
+    const matches = diceGrouped.filter((d) => {
+        if (d.count>1) { return true; }
+        return false;
+    });
+
+   return matches;
+
+}
+
+const groupDice = (dice) => {
+
+    dice.sort();
+
+    const diceGrouped = [
+        {id: 1, "count": 0},
+        {id: 2, "count": 0},
+        {id: 3, "count": 0},
+        {id: 4, "count": 0},
+        {id: 5, "count": 0},
+        {id: 6, "count": 0},
+    ];
+
+    dice.forEach(d => {
+        diceGrouped[d-1].count += 1;
+    });
+
+    return diceGrouped;
+
+}
+
+const ofAKind = (dice, min) => {
+
+    const matches = getMatches(dice);
+
+    if (matches.length && min === 1) {
+        return false;
+    }
+
+    if (matches && matches.filter(d => d.count >= min).length) {
+        return true;
+    }
+
+    return false;
+}
+
+const yahtzee = (dice) => {
+
+    if (ofAKind(dice,5)) { return true; }
+
+    return false;
+}
+
+const fullHouse = (dice) => {
+
+    if (!ofAKind(dice,4) && ofAKind(dice,3) && getMatches(dice).length === 2) { return true; }
+
+    return false;
+}
+
+const straight = (dice, number) => {
+    
+    if (number === 5 && !ofAKind(dice,2)) { return true; }
+    if (number === 4 && !ofAKind(dice,3)) { return true; }
+
+    return false;
+}
---
+++ src/rules/combinations.test.js
@@ -0,0 +1,71 @@
+import {addAllDice, addNumberedDice, determinePoints} from './combinations.js';
+
+it('🎲  add all dice', () => {
+  const addToSix = addAllDice([1,1,1,1,2]);
+  expect(addToSix).toBe(6);
+});
+
+it('🎲  add matching dice', () => {
+  const addToFour = addNumberedDice([1,1,1,1,2], 1);
+  expect(addToFour).toBe(4);
+});
+
+it('🎲  three of a kind', () => {
+  const three_of_a_kind = determinePoints("three_of_a_kind", [1,1,1,2,3]);
+  expect(three_of_a_kind).toBe(8);
+});
+
+it('🎲  NOT three of a kind', () => {
+  const three_of_a_kind = determinePoints("three_of_a_kind", [1,1,2,2,3]);
+  expect(three_of_a_kind).toBe(0);
+});
+
+it('🎲  four of a kind', () => {
+  const four_of_a_kind = determinePoints("four_of_a_kind", [1,1,1,1,3]);
+  expect(four_of_a_kind).toBe(7);
+});
+
+it('🎲  full house', () => {
+  const full_house = determinePoints("full_house", [1,1,1,2,2]);
+  expect(full_house).toBe(25);
+});
+
+it('🎲  NOT full house', () => {
+  const full_house = determinePoints("full_house", [1,1,1,4,5]);
+  expect(full_house).toBe(0);
+});
+
+it('🎲  small', () => {
+  const small_straight = determinePoints("small_straight", [1,1,2,3,4]);
+  expect(small_straight).toBe(30);
+});
+
+it('🎲  large', () => {
+  const large_straight = determinePoints("large_straight", [1,2,3,4,5]);
+  expect(large_straight).toBe(40);
+});
+
+it('🎲  NOT large', () => {
+  const large_straight = determinePoints("large_straight", [1,2,3,5,5]);
+  expect(large_straight).toBe(0);
+});
+
+it('🎲  chance', () => {
+  const chance = determinePoints("chance", [1,2,4,5,6]);
+  expect(chance).toBe(18);
+});
+
+it('🎲  yahtzee', () => {
+  const yahtzee = determinePoints("yahtzee", [1,1,1,1,1]);
+  expect(yahtzee).toBe(50);
+});
+
+it('🎲  number 4x2', () => {
+  const number = determinePoints("number", [1,2,4,4,6], 4);
+  expect(number).toBe(8);
+});
+
+it('🎲  number 4x0', () => {
+  const number = determinePoints("number", [1,2,2,5,6], 0);
+  expect(number).toBe(0);
+});
